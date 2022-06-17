ffe2bcc6f358cf80d7032db17db43561896efb27
JulianGobbato <juliangobbato@hotmail.com>
2022-06-17 12:01:20 -0300

    Building the user interface

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { SharedModule } from './modules/shared.module';
 import { ErrorInterceptor } from './interceptors/error.interceptor';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ServerErrorComponent } from './components/server-error/server-error.component';
+import { MemberCardComponent } from './components/members/member-card/member-card.component';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,8 @@ import { ServerErrorComponent } from './components/server-error/server-error.com
     ListsComponent,
     MessagesComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +43,8 @@ import { ServerErrorComponent } from './components/server-error/server-error.com
     SharedModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
--- client/src/app/modules/shared.module.ts
+++ client/src/app/modules/shared.module.ts
@@ -1,8 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
+import { TabsModule } from 'ngx-bootstrap/tabs'
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 
 
@@ -15,11 +18,16 @@ import { ToastrModule } from 'ngx-toastr';
     ToastrModule.forRoot({
       positionClass:"toast-bottom-right"
     }),
+    TabsModule.forRoot(),
+    HttpClientModule,
+    NgxGalleryModule,
   ],
   exports: [
     TooltipModule,
     BsDropdownModule,
     ToastrModule,
+    TabsModule,
+    NgxGalleryModule,
   ]
 })
 export class SharedModule { }
